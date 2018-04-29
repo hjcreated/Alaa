cdfea459563e74647ba8f4c5dc52f9c24f1ce431
hjcreated <35471644+hjcreated@users.noreply.github.com>
2018-04-29 22:28:19 +0300

    Add files via upload

---
+++ Alaa/Glove/Glove.ino
@@ -0,0 +1,31 @@
+/*Smart Hospital Bed (SHB).
+ * UNIVERSITY OF PETRA .
+ * Graduation project 2017/2018.
+ * This code takes reads from Sensors fixed on the SHB and compute them in order to calculate the pmw to be sent to each Motor.where Speed sensors
+ * calculate how many turns each Motor spin per second,moreover accourding to phisical law (destance = speed / time) we can calculate for how long   
+ * should the wheels turn and how much pwm we should provide. 
+ * the UltraSonic sensors dedict nearby obsticals and decide whether to keep moving or stop.
+ */
+
+ 
+#include "Turn.h"
+// this library reads Gyroscope Data.
+
+Turnlib turn; // instance of turn to use turn library.
+//=============== SETUP METHOD=========================
+void setup() {
+  turn.Setup(); // necessarily to setup GyroScop mpu 5060.
+  Serial.begin(9600);
+}
+
+//=================== LOOP ================================
+void loop() {
+  //start reading from gyroscope 
+turn.readAngle();
+
+
+
+}// LOOP
+
+
+
---
+++ Alaa/sketch_apr20b/sketch_apr20b.ino
@@ -0,0 +1,67 @@
+#include<AFMotor.h>
+AF_DCMotor rightMotor(4); //create motor #1 using M3 output on Motor Drive Shield, set to 1kHz PWM frequency
+AF_DCMotor leftMotor(2); //create motor #2 using M1 output on Motor Drive Shield, set to 1kHz PWM frequency
+char state = 0;
+void setup() {
+pinMode(50,OUTPUT);
+pinMode(52,OUTPUT);
+
+pinMode(52,OUTPUT);
+digitalWrite(52,HIGH); 
+digitalWrite(50,HIGH); 
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available()){
+
+  state = Serial.read();
+if(state == '1'){backward();}
+if(state == '2'){forward();}
+if(state == '3'){right();}
+if(state == '4'){left();}
+if(state == '0'){Stop();}
+}
+
+
+
+
+
+
+}//loop
+void forward(){
+   rightMotor.run(FORWARD);
+   leftMotor.run(FORWARD);
+   rightMotor.setSpeed(150);
+   leftMotor.setSpeed(150);
+       }
+       
+void backward(){
+  rightMotor.run(BACKWARD);
+   leftMotor.run(BACKWARD);
+   rightMotor.setSpeed(150);
+   leftMotor.setSpeed(150);
+   
+  }
+void left(){
+  rightMotor.run(FORWARD);
+   leftMotor.run(BACKWARD);
+   rightMotor.setSpeed(150);
+   leftMotor.setSpeed(150);
+}
+void right(){
+  rightMotor.run(BACKWARD);
+   leftMotor.run(FORWARD);
+   rightMotor.setSpeed(150);
+   leftMotor.setSpeed(150);
+   
+  }
+void Stop(){
+   rightMotor.run(RELEASE);
+   leftMotor.run(RELEASE);
+   rightMotor.setSpeed(0);
+   leftMotor.setSpeed(0);
+   }
+
